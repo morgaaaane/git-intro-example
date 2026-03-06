a7148fe2722af74e9778542d17cafa7be7d723b1
morgaaaane <morgane.vuissoz@student.unisg.ch>
2026-03-06 16:29:21 +0100

    ok

--- forcast.py
+++ forcast.py
@@ -1,5 +1,5 @@
 def forecast_inflation():
-    return "Inflation will be 2% next year in Switzerland."
+    return "Inflation will be 7% next year in Switzerland."
 
 # Run the function and print the result
 result = forecast_inflation()
